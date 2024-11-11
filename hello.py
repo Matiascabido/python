7161562f2e71c512acb9954eb2c8cc0854a7643b
Matiascabido <cabidomatias@gmail.com>
2024-11-10 22:26:17 -0300

    Tipo de datos, comentarios y print

---
+++ hello.py
@@ -0,0 +1,19 @@
+print('Hello Mati')
+
+# Comentario lineal
+"""
+Comentario multiples
+Lineas
+"""
+'''
+  Comentario multilena
+  con coma simple
+'''
+
+# Tipos de datos
+print(type('Soy un String'))    # String
+print(type(3))                  # Int
+print(type(2.3))                # Float
+print(type(1+4j))               # Complex
+print(type(3//4))               # Int
+print(type(True))               # Bool
