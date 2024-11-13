2e8ff619c8f30d4f551f51b72e26a28e20e3f779
Matiascabido <cabidomatias@gmail.com>
2024-11-12 21:05:32 -0300

    se agrega tuplas

--- bases/tuplas.py
+++ bases/tuplas.py
@@ -7,3 +7,31 @@ print(verduleria)
 print(type(verduleria))
 print(verduleria[2])
 print(verduleria[-1])
+
+# Busqueda de vairables repetidas
+print('TOTAL VERDU: ', verduleria.count('papa'))
+
+# Ubicacion de una variable en la tupla
+print('UBICACION PERA: ', verduleria.index('pera'))
+
+# Nuevo conjunto con otra tupla
+otra_verdu = ('zanahoria', 23, 'pepino', 40)
+print('LAS VERDU: ', verduleria + otra_verdu)
+
+# Sub conjunto de una tupla
+otra_verdu = ('zanahoria', 23, 'pepino', 40)
+print('LAS VERDU 2: ', otra_verdu[0:2])
+
+
+# Principales diferencias con las LISTAS
+
+'''
+=> Las tuplas solo tienen dos funciones
+  "index" y "count".
+
+=> Las tuplas no son mutables.
+
+=> La inicializacion de una tupla,
+  se puede identificar por los parentesis () / tupla().
+
+'''
